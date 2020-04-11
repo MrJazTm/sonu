6bd5d40446caf7cd136af6ad69f343294043b451
MrJazTm <63402636+MrJazTm@users.noreply.github.com>
2020-04-11 19:32:17 +0530

    Update templatefly.js

--- templatefly.js
+++ templatefly.js
@@ -1,1 +1,1 @@
-document.write('<div class="templatefly"><a href="https://www.mrjaz.com/">1</a><a href="https://templatefly.mrjaz.com/">2</a></div>')
+document.write('<div class="templatefly"><a href="dddd">1</a><a href="https://templatefly.mrjaz.com/">2</a></div>')
